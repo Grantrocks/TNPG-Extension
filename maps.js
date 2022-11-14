5c80dcf670b872688ca68e97132b484559faa6a2
Grant McNamara <Grantmac12345678911@outlook.com>
2022-11-14 21:45:24 +0000

    asdf

--- maps.js
+++ maps.js
@@ -95,61 +95,19 @@ const goingup = 'd                                                              
             'd                                                                                                                                                                                d      c     c   cc        c     c d.'+
             'd   2                                                                                                                                                                            d      ccccccc   c   c     ccccc   d.'+
             'lbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbr.';
-const level=`d                                                                                                                                                                                                                                                                                                                                                      d.`+
-          'd                                                                                                                                                                                                                                                                                                                                                      d.'+
-          'd                                                                                                                                                                                                                                                                                                                                                      d.'+
-          'd                                                                                                                                                                                                                                                                                                                                                      d.'+
-          'd                                                                                                                                                                                                                                                                                                                                                      d.'+
-          'd                                                                                                                                                                                                                                                                                                                                                      d.'+
-          'd                                                                                                                                                                                                                                                                                                                                                      d.'+
-          'd                                                                                                                                                                                                                                                                                                                                                      d.'+
-          'd                                                                                                                                                                                                                                                                                                                                                      d.'+
-          'd                                                                                                                                                                                                                                                                                                                                                      d.'+
-          'd                                                                                                                                                                                                                                                                                                                                                      d.'+
-          'd                                                                                                                                                                                                                                                                                                                                                      d.'+
-          'd                                                                                                                                                                                                                                                                                                                                                      d.'+
-          'd                                                                                                                                                                                                                                                                                                                                                      d.'+
-          'd                                                                                                                                                                                                                                                                                                                                                      d.'+
-          'd                                                                                                                                                                                                                                                                                                                                                      d.'+
-          'd                                                                                                                                                                                                                                                                                                                                                      d.'+
-          'd                                                                                                                                                                                                                                                                                                                                                      d.'+
-          'd                                                                                                                                                                                                                                                                                                                                                      d.'+
-          'd                                                                                                                                                                                                                                                                                                                                                      d.'+
-          'd                                                                                                                                                                                                                                                                                                                                                      d.'+
-          'd                                                                                                                                                                                                                                                                                                                                                      d.'+
-          'd                                                                                                                                                                                                                                                                                                                                                      d.'+
-          'd                                                                                                                                                                                                                                                                                                                                                      d.'+
-          'd                                                                                                                                                                                                                                                                                                                                                      d.'+
-          'd                                                                                                                                                                                                                                                                                                                                                      d.'+
-          'd                                                                                                                                                                                                                                                                                                                                                      d.'+
-          'd                                                                                                                                                                                                                                                                                                                                                      d.'+
-          'd                                                                                                                                                                                                                                                                                                                                                      d.'+
-          'd                                                                                                                                                                                                                                                                                                                                                      d.'+
-          'd                                                                                                                                                                                                                                                                                                                                                      d.'+
-          'd                                                                                                                                                                                                                                                                                                                                                      d.'+
-          'lddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddr.'
-      
-      
-      
-      
-      
-      
-      /*'dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd.'+
-            'd                                                                        d.'+
-            'd    c     s     c                                                       d.'+
-            'd 2  p     p     p     p                                                 d.'+
-            'd p                                                                      d.'+
-            'd       p                                                                d.'+
-            'd                                                                        d.'+
-            'd    p     p     p  c  p                             c       sc          d.'+
-            'd p           p     p                                p      ppp          d.'+
-            'd       c                                 c s    pp                      d.'+
-            'd               c                         ppp                            d.'+
-            'd    p     p     p     p              s                                  d.'+
-            'd p     p           p                 p                                  d.'+
-            'd                                p                                       d.'+
-            'd ccccccccccc   s    s         c                                         d.'+
-            'lbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbr';*/
-
-
-            
+const level='d                                                 d.'+
+            'd                                                 d.'+
+            'd                                                 d.'+
+            'd                                                 d.'+
+            'd                                                 d.'+
+            'd                                                 d.'+
+            'd                                                 d.'+
+            'd                                                 d.'+
+            'd                                                 d.'+
+            'd                                                 d.'+
+            'd                                                 d.'+
+            'd                                                 d.'+
+            'd                                                 d.'+
+            'd                                                 d.'+
+            'd                                                 d.'+
+            'ldddddddddddddddddddddddddddddddddddddddddddddddddr.'
